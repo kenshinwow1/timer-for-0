6f2a0b33712bf99ed8f96a888a7a6c8913dd3282
Ualink <kenshinwow1@gmail.com>
2024-01-17 00:06:02 +0100

    clock

--- python.py
+++ python.py
@@ -1,10 +1,32 @@
-import random
+from tkinter import *
+from time import *
 
-random_num = random.randint(1, 5)
-while True:
-   num = int(input("INPUT NUMBER 1-5 : "))
-   if num != random_num:
-       print("Try more ;)")
-       continue
-   print("u the KING11!", random_num)
-   break
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
+    window.after(1000,update)
+
+
+window = Tk()
+
+time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
+time_label.pack()
+
+day_label = Label(window,font=("Ink Free",25,"bold"))
+day_label.pack()
+
+date_label = Label(window,font=("Ink Free",30))
+date_label.pack()
+
+update()
+
+window.mainloop()
+
+#clock
